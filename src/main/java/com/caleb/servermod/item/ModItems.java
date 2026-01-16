16f6791206344e6ea824aa4c3a3d1228542a4948
caleb yung <caleb.pierre.yung2@gmail.com>
2026-01-16 12:37:34 -0500

    Redundant code
    Got rid of the Example block
    as well as changed the name of the variable for the copper coin

--- src/main/java/com/caleb/servermod/item/ModItems.java
+++ src/main/java/com/caleb/servermod/item/ModItems.java
@@ -24,20 +24,20 @@ public class ModItems {
     // Create a Deferred Register to hold CreativeModeTabs which will all be registered under the "examplemod" namespace
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, Servermod.MODID);
 
-    // Creates a new Block with the id "examplemod:example_block", combining the namespace and path
-    public static final RegistryObject<Block> EXAMPLE_BLOCK = BLOCKS.register("example_block", () -> new Block(BlockBehaviour.Properties.of().mapColor(MapColor.STONE)));
-    // Creates a new BlockItem with the id "examplemod:example_block", combining the namespace and path
-    public static final RegistryObject<Item> EXAMPLE_BLOCK_ITEM = ITEMS.register("example_block", () -> new BlockItem(EXAMPLE_BLOCK.get(), new Item.Properties()));
+    // // Creates a new Block with the id "examplemod:example_block", combining the namespace and path
+    // public static final RegistryObject<Block> EXAMPLE_BLOCK = BLOCKS.register("example_block", () -> new Block(BlockBehaviour.Properties.of().mapColor(MapColor.STONE)));
+    // // Creates a new BlockItem with the id "examplemod:example_block", combining the namespace and path
+    // public static final RegistryObject<Item> EXAMPLE_BLOCK_ITEM = ITEMS.register("example_block", () -> new BlockItem(EXAMPLE_BLOCK.get(), new Item.Properties()));
 
-    // Creates a new food item with the id "examplemod:example_id", nutrition 1 and saturation 2
+    // // Creates a new food item with the id "examplemod:example_id", nutrition 1 and saturation 2
 
-    public static final RegistryObject<Item> Copper_Coin = ITEMS.register("copper_coin", () -> new Item(new Item.Properties().stacksTo(64)));
+    public static final RegistryObject<Item> COPPER_COIN = ITEMS.register("copper_coin", () -> new Item(new Item.Properties().stacksTo(64)));
 
     // Creates a creative tab with the id "examplemod:example_tab" for the example item, that is placed after the combat tab
     public static final RegistryObject<CreativeModeTab> EXAMPLE_TAB = CREATIVE_MODE_TABS.register("example_tab", () -> CreativeModeTab.builder()
             .withTabsBefore(CreativeModeTabs.COMBAT)
             .title(Component.translatable("creativetab.servermod")  )
-            .icon(() -> Copper_Coin.get().getDefaultInstance())
+            .icon(() -> COPPER_COIN.get().getDefaultInstance())
             .displayItems((parameters, output) -> {
                 output.accept(EXAMPLE_BLOCK_ITEM.get());
                 output.accept(Copper_Coin.get()); // Add the example item to the tab. For your own tabs, this method is preferred over the event
